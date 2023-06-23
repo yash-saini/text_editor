e10053f4c8ac5e22bc224fd309f9ff817af6ac76
Yash Saini <yash.0245113@gmail.com>
2023-06-23 18:08:47 +0530

    Dark Mode in Navbar

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Navbar from './components/Navbar';
 import Txtform from './components/Txtform';
 
 function App() {
+
+
   return (
     <>
     <Navbar title="TextChanger" about="About"/>
-    <About></About>
+    {/* <About></About> */}
     <div className= "container">
     <Txtform heading="Your text goes here" />
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,39 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { useState } from 'react'
+
 
 export default function Navbar(props) {
+    const[buttonlabel,changelabel] = useState("Dark Mode On")
+
+    const[dorl,navbarchange]=useState('light')
+    
+    const darkmode = () =>{
+        if (dorl==='light')
+        {
+            navbarchange('dark')
+            changelabel("Dark Mode on")
+        }
+        else
+        {
+            navbarchange('light')
+            changelabel("Light Mode on")
+        }
+        
+    }
+    
+
+
+ 
   return (
     
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div className="container-fluid">
+    <nav className={`navbar navbar-expand-lg navbar-${dorl} bg-${dorl}`}>
+  <div className="container-fluid" >
     <a className="navbar-brand mb-0.5" href="/">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <div className="collapse navbar-collapse" id="navbarSupportedContent" >
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
           <a className="nav-link active" aria-current="page" href="/">Home</a>
@@ -19,14 +42,22 @@ export default function Navbar(props) {
           <a className="nav-link" href="/">{props.about}</a>
         </li>
       </ul>
-{/*       
+  
+     {/*       
       <form className="d-flex" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
         <button className="btn btn-outline-primary" type="submit">Search</button>
       </form>
        */}
+       <div className="form-check form-switch" >
+            <input className="form-check-input" type="checkbox" onClick={darkmode} role="switch" id="flexSwitchCheckDefault"/>
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault" style={{color : dorl ==='light' ? 'black':'white'}}>{buttonlabel}</label>
     </div>
+
+    </div>
+
   </div>
+ 
 </nav>
 
   )
